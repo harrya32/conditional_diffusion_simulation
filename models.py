64c35a0c5d4da17cd7ad7836789b1f4f161fae89
Harry Amad <harryamad@Harrys-MacBook-Pro-3.local>
2023-08-04 18:13:24 +0100

    added config python files and cleaned up models

---
+++ models.py
@@ -0,0 +1,208 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+import torch.nn.functional as F
+
+
+
+#Implimentation from shi et al - best model so far :)
+class ScoreNet_2D(nn.Module):
+    
+    def __init__(self, marginal_prob_std):
+        super().__init__()
+        self.marginal_prob_std = marginal_prob_std
+        
+        self.net = MLP(3 * 32,
+                       layer_widths=[128,128] + [2],
+                       activate_final = False,
+                       activation_fn=torch.nn.LeakyReLU())
+
+        self.t_encoder = MLP(16,
+                             layer_widths=[16] + [32],
+                             activate_final = True,
+                             activation_fn=torch.nn.LeakyReLU())
+
+        self.xy_encoder = MLP(2,
+                              layer_widths=[32] + [64],
+                              activate_final = True,
+                              activation_fn=torch.nn.LeakyReLU())
+        
+    def forward(self, x):
+        t = x[:, -1]
+        norm = self.marginal_prob_std(t)[:, None]
+        t = t.reshape(-1,1)
+        xy = x[:,[0,1]]
+        
+        t_emb = get_timestep_embedding(t, 16, 10000)
+        t_emb = self.t_encoder(t_emb)
+        xy_emb = self.xy_encoder(xy)
+
+        
+        h = torch.cat([xy_emb, t_emb], -1)
+
+        out = self.net(h) 
+
+        out = out / norm
+        return out
+    
+
+class ScoreNet_BOD(nn.Module):
+    
+    def __init__(self, marginal_prob_std):
+        super().__init__()
+        self.marginal_prob_std = marginal_prob_std
+        
+        self.net = MLP(3 * 32,
+                       layer_widths=[128,128] + [7],
+                       activate_final = False,
+                       activation_fn=torch.nn.LeakyReLU())
+
+        self.t_encoder = MLP(16,
+                             layer_widths=[16] + [32],
+                             activate_final = True,
+                             activation_fn=torch.nn.LeakyReLU())
+
+        self.xy_encoder = MLP(2 + 5,
+                              layer_widths=[32] + [64],
+                              activate_final = True,
+                              activation_fn=torch.nn.LeakyReLU())
+        
+    def forward(self, x):
+        t = x[:, -1]
+        norm = self.marginal_prob_std(t)[:, None]
+        t = t.reshape(-1,1)
+        xy = x[:,[0,1,2,3,4,5,6]]
+        
+        t_emb = get_timestep_embedding(t, 16, 10000)
+        t_emb = self.t_encoder(t_emb)
+        xy_emb = self.xy_encoder(xy)
+
+        
+        h = torch.cat([xy_emb, t_emb], -1)
+
+        out = self.net(h) 
+
+        out = out / norm
+        return out
+    
+    
+def get_timestep_embedding(timesteps, embedding_dim=128, max_period=10000):
+    """
+      From Fairseq.
+      Build sinusoidal embeddings.
+      This matches the implementation in tensor2tensor, but differs slightly
+      from the description in Section 3.5 of "Attention Is All You Need".
+      https://github.com/pytorch/fairseq/blob/master/fairseq/modules/sinusoidal_positional_embedding.py
+    """
+    half_dim = embedding_dim // 2
+    emb = math.log(max_period) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim, device=timesteps.device) * -emb)
+
+    emb = timesteps * emb.unsqueeze(0)
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=-1)
+    if embedding_dim % 2 == 1:  # zero pad
+        emb = F.pad(emb, [0,1])
+
+    return emb
+
+class MLP(torch.nn.Module):
+    def __init__(self, input_dim, layer_widths, activate_final = False, activation_fn=F.relu):
+        super(MLP, self).__init__()
+        layers = []
+        prev_width = input_dim
+        for layer_width in layer_widths:
+            layers.append(torch.nn.Linear(prev_width, layer_width))
+            # # same init for everyone
+            # torch.nn.init.constant_(layers[-1].weight, 0)
+            prev_width = layer_width
+        self.input_dim = input_dim
+        self.layer_widths = layer_widths
+        self.layers = torch.nn.ModuleList(layers)
+        self.activate_final = activate_final
+        self.activation_fn = activation_fn
+        
+    def forward(self, x):
+        for i, layer in enumerate(self.layers[:-1]):
+            x = self.activation_fn(layer(x))
+        x = self.layers[-1](x)
+        if self.activate_final:
+            x = self.activation_fn(x)
+        return x
+    
+
+
+def loss_fn(model, x, marginal_prob_std, eps=1e-5):
+    """The loss function for training score-based generative models.
+
+    Args:
+    model: A PyTorch model instance that represents a 
+      time-dependent score-based model.
+    x: A mini-batch of training data.    
+    marginal_prob_std: A function that gives the standard deviation of 
+      the perturbation kernel.
+    eps: A tolerance value for numerical stability.
+    """
+    random_t = torch.rand(x.shape[0]) * (1. - eps) + eps  
+    std = marginal_prob_std(random_t)
+    random_t = torch.reshape(random_t, (x.shape[0], 1))
+    z = torch.randn_like(x)
+    perturbed_x = x + z * std[:, None]
+    x_with_t = torch.hstack([perturbed_x,random_t])
+    x_with_t = x_with_t.to(torch.float32)
+    score = model(x_with_t)
+    loss = torch.mean(torch.sum((score * std[:, None] + z)**2, dim=0))
+    return loss
+
+
+##############
+##############
+##############
+
+class ScoreNet_old(nn.Module):
+    
+    def __init__(self, marginal_prob_std):
+        super().__init__()
+        self.act = nn.GELU()#lambda x: x * torch.sigmoid(x)
+        self.marginal_prob_std = marginal_prob_std
+        self.hidden_units = 256
+        # inputs of data (x,y), timestep t
+        self.mlp = nn.Sequential(
+            nn.Linear(3, int(self.hidden_units), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units), int(self.hidden_units/2), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/2), int(self.hidden_units/4), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/4), int(self.hidden_units/8), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/8), int(self.hidden_units/16), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/16), int(self.hidden_units/8), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/8), int(self.hidden_units/4), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/4), int(self.hidden_units/2), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units/2), int(self.hidden_units), bias=True),
+            nn.GELU(),
+            nn.Linear(int(self.hidden_units), 2, bias=True)
+        )
+    
+    def forward(self, x):
+        t = x[:, 2]
+        x = self.mlp(x)
+        x = x / self.marginal_prob_std(t)[:, None]
+        return x
+    
+class GaussianFourierProjection(nn.Module):
+    """Gaussian random features for encoding time steps."""  
+    def __init__(self, embed_dim, scale=30.):
+        super().__init__()
+        # Randomly sample weights during initialization. These weights are fixed 
+        # during optimization and are not trainable.
+        self.W = nn.Parameter(torch.randn(embed_dim // 2) * scale, requires_grad=False)
+    
+    def forward(self, x):
+        x_proj = x[:, None] * self.W[None, :] * 2 * np.pi
+        return torch.cat([torch.sin(x_proj), torch.cos(x_proj)], dim=-1)
